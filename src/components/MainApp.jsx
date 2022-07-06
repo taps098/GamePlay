3cd220cdcb558034820fc5648ca437bca05a9b5e
Taps B <63951156+taps098@users.noreply.github.com>
2022-07-06 23:56:46 +0530

    router changes done

--- src/components/MainApp.jsx
+++ src/components/MainApp.jsx
@@ -1,39 +1,15 @@
 import React, {useState} from 'react';
-import ColourGame from './colourGame/ColourGame';
-import AboutPage from './aboutPage/aboutPage';
-import ContentPage from './contentPage/ContentPage';
-
+import { Link } from "react-router-dom";
 
 const MainApp = () => {
-    const [showAbout, setShowAbout] = useState(false);
-    const [contentFlag, setContentFlag] = useState(false);
-    const clickTicTacToeHandler = (e) => {
-        console.log('tic tac toe is clicked', e);
-    }
-    const handleAboutPage = () => {
-        console.log('click about');
-        setShowAbout(true);
-        setContentFlag(false);
-    }
-    const handleContent = () => {
-        console.log('game starting');
-        setShowAbout(false);
-        setContentFlag(true);
-    }
   return (
     <div>
-        <div className='sidenav'>
-            <button onClick={handleAboutPage}>About</button>
-            <button onClick={handleContent}>Colour game</button>
-            <button>Tic tac toe</button>
-            <button>Contact</button>
-        </div>
-        <div className='contentPageStyle'>
-            {showAbout ? <AboutPage />: null}
-            {contentFlag? <ContentPage/>: null}
-        </div>
+        <nav className='sidenav'>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/game">Games</Link>
+        </nav>
     </div>
-    
   );
 }
 
